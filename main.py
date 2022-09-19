a8246c0e6903b61993908667d1a7847427ddf52c
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-09-19 16:27:11 +0530

    initial push

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 print("hello world")
 
-print("this is adhitya testing if pycharm is pushing files properly or not");
+print("this is adhitya testing if pycharm is pushing files properly or not")
+
+# this is the main.py where the fundamentals are tested
+
+
+# ..
+
+# ok this is working..
+
+
+# Intro
+
+class Myself:
+    def __init__(self):
+        self.name = "Adhitya"
+        self.age = 19
+        self.alive = True
+
+
+
+
+
+
+
+
+
+
+
