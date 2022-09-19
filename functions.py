c6779c26f9fc9701dc46d3ce4b87dbe33736afbd
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-09-19 16:48:06 +0530

    initial push

--- functions.py
+++ functions.py
@@ -1,2 +1,22 @@
+# short program Guess the number
 
-print("hello world")
+import random
+
+i = 0
+num = random.randint(0, 30)
+
+print("guess the number : ")
+
+while True:
+    guess = int(input())
+    i = i + 1
+    if guess == num:
+        print("congragulations, you have guessed the correct number in ", i, " chances")
+        break
+    if guess < num:
+        print("low")
+
+    if guess > num:
+        print("high")
+
+
